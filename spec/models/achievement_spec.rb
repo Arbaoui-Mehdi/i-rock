ac2d3258459728d01a4e45fa0e24668343055098
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-12 05:53:37 -0700

    Test Validations

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -2,4 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
 
+  #
+  #
+  # Validations
+  describe 'validations' do
+
+    it 'requires titles' do
+      achievement = Achievement.new(title: '')
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it 'requires title to be unique for one user' do
+      user = FactoryGirl.create(:user)
+      first_achievement = FactoryGirl.create(
+        :private_achievement,
+        title: 'First Achievement',
+        user: user
+      )
+      new_achievement = Achievement.new(
+        title: 'First Achievement',
+        user: user
+      )
+
+      expect(new_achievement.valid?).to be_falsy
+    end
+
+    it 'allows different users to have achievements with identical titles' do
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      first_achievement = FactoryGirl.create(
+        :public_achievement,
+        title: 'First Achievement',
+        user: user1
+      )
+      new_achievement = FactoryGirl.create(
+        :public_achievement,
+        title: 'First Achievement',
+        user: user2
+      )
+
+      expect(new_achievement.valid?).to be_truthy
+    end
+
+  end
+
 end
